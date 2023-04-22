9f561a00025fd77dc09961f53f317b97a0cbbb8e
Spencer Hahmo <schahmo@gmail.com>
2023-04-21 21:37:59 -0600

    Retrieving Interviewer Data. Refactor getAppointmentsForDay so the application doesn't compile with errors

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,13 +5,14 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -20,21 +21,26 @@ export default function Application(props) {
     Promise.all([
       Promise.resolve(axios.get('http://localhost:8001/api/days')),
       Promise.resolve(axios.get('http://localhost:8001/api/appointments')),
+      Promise.resolve(axios.get('http://localhost:8001/api/interviewers')),
     ])
     .then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const schedule = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
-    )
+    );
   });
 
   return (
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,11 +1,27 @@
 export function getAppointmentsForDay(state, day) {
   const appointments = [];
+  const currentDay = state.days.filter(days => days.name === day);
 
-  state.days.map(days => {
-    if (days.name === day) {
-      days.appointments.filter(appointment => appointments.push(state.appointments[appointment]));
-    }
-  });
+  if (!currentDay[0]) {
+    return appointments;
+  }
+
+  for (const appointment of currentDay[0].appointments) {
+    appointments.push(state.appointments[appointment]);
+  }
 
   return appointments;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviews = state.interviewers[interview.interviewer];
+  
+  return {
+    student: interview.student,
+    interviewer: interviews,
+  };
 }
